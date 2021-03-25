5fe9a23e88ccc27f736db099a51060d069ad2e8f
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-03-25 00:51:26 +0000

    Add test routes for the Stop loss limit for buy and sell order sides

--- src/account.rs
+++ src/account.rs
@@ -515,6 +515,38 @@ impl Account {
         Ok(transaction)
     }
 
+    /// Place a test stop limit sell order
+    ///
+    /// This order is sandboxed: it is validated, but not sent to the matching engine.
+    pub fn test_stop_limit_buy_order<S, F>(
+        &self,
+        symbol: S,
+        qty: F,
+        price: f64,
+        stop_price: f64,
+        time_in_force: TimeInForce,
+    ) -> Result<()>
+    where
+        S: Into<String>,
+        F: Into<f64>,
+    {
+        let sell: OrderRequest = OrderRequest {
+            symbol: symbol.into(),
+            qty: qty.into(),
+            price,
+            stop_price: Some(stop_price),
+            order_side: OrderSide::Sell,
+            order_type: OrderType::StopLossLimit,
+            time_in_force: time_in_force,
+        };
+        let order = self.build_order(sell);
+        let request = build_signed_request(order, self.recv_window)?;
+        let data = self.client.post_signed(API_V3_ORDER_TEST, &request)?;
+        let _: TestResponse = from_str(data.as_str())?;
+
+        Ok(())
+    }
+
     /// Place a stop limit buy order
     pub fn stop_limit_buy_order<S, F>(
         &self,
@@ -543,6 +575,38 @@ impl Account {
         let transaction: Transaction = from_str(data.as_str())?;
 
         Ok(transaction)
+    }
+
+    /// Place a test stop limit buy order
+    ///
+    /// This order is sandboxed: it is validated, but not sent to the matching engine.
+    pub fn test_stop_limit_buy_order<S, F>(
+        &self,
+        symbol: S,
+        qty: F,
+        price: f64,
+        stop_price: f64,
+        time_in_force: TimeInForce,
+    ) -> Result<()>
+    where
+        S: Into<String>,
+        F: Into<f64>,
+    {
+        let sell: OrderRequest = OrderRequest {
+            symbol: symbol.into(),
+            qty: qty.into(),
+            price,
+            stop_price: Some(stop_price),
+            order_side: OrderSide::Buy,
+            order_type: OrderType::StopLossLimit,
+            time_in_force: time_in_force,
+        };
+        let order = self.build_order(sell);
+        let request = build_signed_request(order, self.recv_window)?;
+        let data = self.client.post_signed(API_V3_ORDER_TEST, &request)?;
+        let _: TestResponse = from_str(data.as_str())?;
+
+        Ok(())
     }
 
     /// Place a custom order
